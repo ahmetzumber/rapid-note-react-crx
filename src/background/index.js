804d888460820a872c8f3db868d9d17da94810ff
ahmetzumber <ahmetzumber5@gmail.com>
2022-03-15 16:36:15 +0300

    template converted to rapid note crx

--- src/background/index.js
+++ src/background/index.js
@@ -1,44 +1,81 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, child, get } from "firebase/database";
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs, doc, setDoc } from "firebase/firestore"
 
 // Set the configuration for your app
 // TODO: Replace with your project's config object
 const firebaseConfig = {
-    apiKey: ".....",
-    authDomain: ".....firebaseapp.com",
-    databaseURL: "https://<YOUR FIREBASE>.firebaseio.com ",
-    projectId: "......",
-    storageBucket: "......appspot.com",
-    messagingSenderId: ".....",
-    appId: "1:.....:web:......"
+    apiKey: "AIzaSyCAAnIWGjZ4eczEqvmcxlOEb1ZvZAIVwUY",
+    authDomain: "note-application-bbacc.firebaseapp.com",
+    databaseURL: "https://note-application-bbacc-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "note-application-bbacc",
+    storageBucket: "note-application-bbacc.appspot.com",
+    messagingSenderId: "813349799108",
+    appId: "1:813349799108:web:bc44d2552bd63a7faeb591",
+    measurementId: "G-71ETHEM64C"
   };
-const app = initializeApp(firebaseConfig);
 
-// FIREBASE DATABASE
-const dbRef = ref(getDatabase(app));
-get(child(dbRef, `/`)).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
-
-//FREBASE AUTH
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
-    console.log('User logged in uid - ', uid)
-  } else {
-    // User is signed out
-    console.log('User is not logged in')
-  }
-});
+initializeApp(firebaseConfig);
+const db = getFirestore();
 
 console.log('background script logic here...')
+
+
+chrome.runtime.onInstalled.addListener(async () => {
+
+  const projectCollection = await getDocs(collection(db, "project"));
+
+  let contextItems = [];
+  contextItems.push({ id: 'create', title: 'Save as a New Project'});
+
+  projectCollection.forEach((doc) => {
+    let obj = { id: doc.id, title: "Save to "+doc.id }
+    contextItems.push(obj);
+    obj = {}    
+  });
+
+
+  for (let i in contextItems){
+    chrome.contextMenus.create({
+      "id": contextItems[i].id,
+      "title": contextItems[i].title,
+      "type": "normal",
+      "contexts": ["selection"],
+    })
+  }
+
+  chrome.contextMenus.onClicked.addListener( async readData => {
+
+      if (readData.menuItemId === "create") {
+
+        let message = 'Please enter a Project Name'
+	      let result = window.prompt(message);
+
+        try {
+          const docRef = await setDoc(doc(db, "project", result), {
+            data: readData.selectionText,
+            data_url: readData.pageUrl
+          });
+
+          if (docRef !== null) 
+            window.open('http://localhost:8081/')
+          
+          console.log("Written document: ", docRef);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+
+               
+      }
+      else {
+        console.log(readData.selectionText)
+        console.log(readData.pageUrl)
+
+        await setDoc(doc(db, "project", readData.menuItemId), {
+          data: readData.selectionText,
+          data_url: readData.pageUrl
+        });
+
+      }
+  }) // contextMenus.onClicked
+}) // chrome.runtime.onInstalled
+
